83f0a36777ff5297cdd56149b059a64e639e4d7a
Michael Guerga <mguerga@c1r8s1.42lausanne.ch>
2023-04-25 14:08:50 +0200

    revert last reprompt changes

--- src/mini_main.c
+++ src/mini_main.c
@@ -6,7 +6,7 @@
 /*   By: mguerga <mguerga@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/20 20:56:43 by mguerga           #+#    #+#             */
-/*   Updated: 2023/04/24 12:04:38 by mguerga          ###   ########.fr       */
+/*   Updated: 2023/04/20 20:57:59 by mguerga          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,29 +21,21 @@
 
 int	main(int ac, char *av[], char *env[])
 {
-	(void) av;
-	if (ac != 1)
-		return (1);
-	ft_signals();
-	(void)env;
-	//minish.env = ft_copy_env(env);
-	//if (minish.env == NULL)
-	//	return (1);
-	prompter();
-	return (0);
-}
-
-void	prompter(void)
-{
 	t_minish	minish;
 
+	(void) av;
+	ft_signals();
+	if (ac != 1)
+		return (1);
+	minish.env = ft_copy_env(env);
+	if (minish.env == NULL)
+		return (1);
 	while (1)
 	{
-		if (minish.line == NULL)
-			exit(0);
 		minish.line = readline("(ಠ.ಠ)¬ ");
 		add_history(minish.line);
-//		ft_parse(minish);
+//		ft_parse(&minish);
 		free(minish.line);
 	}
+	return (0);
 }
